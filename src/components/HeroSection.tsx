03fc614a723ad9853d8d7fa37b4293dffdca6753
Asser Walaa <111322681+AsserrWalaa@users.noreply.github.com>
2024-12-30 12:13:02 +0200

    Add files via upload

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -17,9 +17,13 @@ const Hero = () => {
           <h2 className="pt-3 my-5" style={{ color: "#676767" }}>
             {i18n.t("Health and cleanliness")}
           </h2>
-          <button className="w-50 fs-3 px-4 rounded-3 border-3 bg-transparent callUs">
-            {i18n.t("Call Us")}
-          </button>
+          <a
+            href="tel:+201066607106"
+            className="w-50 fs-3 px-4 rounded-3 border-3 bg-transparent callUs text-decoration-none">
+            <button className="w-100 h-100 border-0 bg-transparent text-dark">
+              {i18n.t("Call Us")}
+            </button>
+          </a>
         </div>
         <div className="hero col-12 col-md-6">
           <img src={hero1} alt="" className="img-fluid d-none d-md-block" />
